39f67f33802974f68707f52c3796b31717afe777
MalinduSasanga <lkmsasanga@gmail.com>
2021-10-13 11:43:31 +0530

    onesignal module added

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,25 @@
             appId: "96aff1fa-2988-4e6e-945b-38ac82b1b89c"
           });
         });
+
+        // Inside page components
+        this.$OneSignal.push(() => {
+          this.$OneSignal.isPushNotificationsEnabled(isEnabled => {
+            if (isEnabled) {
+              console.log("Push notifications are enabled!");
+            } else {
+              console.log("Push notifications are not enabled yet.");
+            }
+          });
+        });
+
+        // Using window and array form
+        window.$OneSignal.push([
+          "addListenerForNotificationOpened",
+          data => {
+            console.log("Received NotificationOpened:", data);
+          }
+        ]);
       </script>
     </head>
     <h1>One signal web push notification testing project</h1>
