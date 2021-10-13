f9c1d08400df7edff55760f715b015687d435782
MalinduSasanga <lkmsasanga@gmail.com>
2021-10-13 09:12:22 +0530

    onesignal configurations added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <Tutorial/>
+  <div>
+    <head>
+      <script
+        src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+        async=""
+      ></script>
+      <script>
+        window.OneSignal = window.OneSignal || [];
+        OneSignal.push(function() {
+          OneSignal.init({
+            appId: "d5149721-0cf8-4dd2-8066-f0b1949dd7dc"
+          });
+        });
+      </script>
+    </head>
+    <Tutorial />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
